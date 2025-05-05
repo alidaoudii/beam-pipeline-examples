01afcbe0711efcedcf3282e6488a4f713dca3b44
alidaoudii <daoudirabi3@gmail.com>
2025-05-05 17:03:29 +0200

    Update LocalOptions.java

--- src/main/java/com/harland/example/batch/LocalOptions.java
+++ src/main/java/com/harland/example/batch/LocalOptions.java
@@ -1,17 +1,53 @@
 package com.harland.example.batch;
 
-import org.apache.beam.sdk.options.Description;
-import org.apache.beam.sdk.options.PipelineOptions;
-import org.apache.beam.sdk.options.Validation;
+import com.harland.example.common.model.TransferRecord;
+import com.harland.example.common.transform.ConvertToTransferRecordFn;
+import com.harland.example.common.utils.MathUtils;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.Sum;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.TypeDescriptors;
 
-public interface LocalOptions extends PipelineOptions {
-    @Description("Path of the input CSV file")
-    @Validation.Required
-    String getInput();
-    void setInput(String value);
+// Utilisation de l'interface LocalOptions pour les options locales
+public class BigQueryImportPipeline {
 
-    @Description("Prefix for the output files")
-    @Validation.Required
-    String getOutput();
-    void setOutput(String value);
+    public static void main(String[] args) {
+        // Parse arguments en LocalOptions (input et output)
+        LocalOptions options = PipelineOptionsFactory
+            .fromArgs(args)
+            .withValidation()
+            .as(LocalOptions.class);
+
+        Pipeline pipeline = Pipeline.create(options);
+
+        pipeline
+            // Lecture du fichier CSV local
+            .apply("ReadCSV", TextIO.read().from(options.getInput()))
+            // Conversion en TransferRecord
+            .apply("ToRecord", ParDo.of(new ConvertToTransferRecordFn()))
+            // Création de paires KV<user, amount>
+            .apply("ToKV",
+                MapElements.into(
+                    TypeDescriptors.kvs(TypeDescriptors.strings(), TypeDescriptors.doubles()))
+                .via(record -> KV.of(record.getUser(), record.getAmount())))
+            // Somme des montants par utilisateur
+            .apply("SumAmounts", Sum.doublesPerKey())
+            // Formatage en lignes CSV
+            .apply("FormatCSV",
+                MapElements.into(TypeDescriptors.strings())
+                .via(kv -> kv.getKey() + "," + MathUtils.roundToTwoDecimals(kv.getValue())))
+            // Écriture dans un unique fichier local
+            .apply("WriteCSV",
+                TextIO.write()
+                      .to(options.getOutput())
+                      .withSuffix(".csv")
+                      .withoutSharding());
+
+        // Exécute et attend la fin
+        pipeline.run().waitUntilFinish();
+    }
 }
