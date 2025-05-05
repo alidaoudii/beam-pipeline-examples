4ae034898ad16f2443a6655795baa88dcfdd45bc
alidaoudii <daoudirabi3@gmail.com>
2025-05-05 16:18:43 +0200

    Update BigQueryImportPipeline.java

--- src/main/java/com/harland/example/batch/BigQueryImportPipeline.java
+++ src/main/java/com/harland/example/batch/BigQueryImportPipeline.java
@@ -1,16 +1,11 @@
 package com.harland.example.batch;
 
-import com.google.api.services.bigquery.model.TableRow;
-import com.harland.example.common.bigquery.Schema;
 import com.harland.example.common.model.TransferRecord;
-import com.harland.example.common.options.AwsOptionsParser;
 import com.harland.example.common.options.BigQueryImportOptions;
 import com.harland.example.common.transform.ConvertToTransferRecordFn;
 import com.harland.example.common.utils.MathUtils;
-import com.harland.example.common.utils.JsonSchemaReader;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
-import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.transforms.ParDo;
@@ -18,60 +13,30 @@ import org.apache.beam.sdk.transforms.Sum;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.TypeDescriptors;
 
-import java.io.IOException;
-
-/**
- * Pipeline for importing CSV data from Google Cloud Storage or AWS S3 and writing it to Google
- * BigQuery.
- */
 public class BigQueryImportPipeline {
 
-  private static final String SCHEMA_FILE = "schema.json";
-
-  public static void main(String... args) throws IOException {
+  public static void main(String[] args) {
     BigQueryImportOptions options =
-        PipelineOptionsFactory.fromArgs(args).as(BigQueryImportOptions.class);
+        PipelineOptionsFactory.fromArgs(args).withValidation().as(BigQueryImportOptions.class);
 
-    // Configure AWS specific options
-    AwsOptionsParser.formatOptions(options);
-
-    runPipeline(options);
-  }
-
-  private static void runPipeline(BigQueryImportOptions options) throws IOException {
     Pipeline p = Pipeline.create(options);
 
-    Schema schema = new Schema(JsonSchemaReader.readSchemaFile(SCHEMA_FILE));
-    String bqColUser = schema.getColumnName(0);
-    String bqColAmount = schema.getColumnName(1);
-
-    // Read local directory 
-    p.apply("ReadFromLocal", TextIO.read().from("data/accidents_2005_2020.csv"))
-
-
-        // Convert our CSV rows into a TransferRecord object.
+    p.apply("ReadFromLocal", TextIO.read().from("ali/accidents_2005_2020.csv"))
         .apply("ConvertToTransferRecord", ParDo.of(new ConvertToTransferRecordFn()))
-
-        // Map our elements into KV pairs by user.
         .apply(
             "CreateKVPairs",
             MapElements.into(
                     TypeDescriptors.kvs(TypeDescriptors.strings(), TypeDescriptors.doubles()))
                 .via((TransferRecord record) -> KV.of(record.getUser(), record.getAmount())))
-
-        // Sum our KV pairs for each user.
         .apply("SumAmountsPerUser", Sum.doublesPerKey())
-
-        // Write the result to BigQuery.
-       .apply(
-      "FormatAsCSV",
-      MapElements.into(TypeDescriptors.strings())
-          .via((KV<String, Double> record) ->
-              record.getKey() + "," + MathUtils.roundToTwoDecimals(record.getValue())))
-  
         .apply(
-            "WriteToLocal",
-            TextIO.write().to("output/results").withSuffix(".csv").withoutSharding());
-      
-      }
+            "FormatResults",
+            MapElements.into(TypeDescriptors.strings())
+                .via(
+                    (KV<String, Double> record) ->
+                        record.getKey() + "," + MathUtils.roundToTwoDecimals(record.getValue())))
+        .apply("WriteToLocal", TextIO.write().to("output/results").withSuffix(".csv").withoutSharding());
+
+    p.run().waitUntilFinish();
+  }
 }
